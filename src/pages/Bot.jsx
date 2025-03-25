7f56b411892eb6f2d74c7508bfdce9b9b8897f33
SriGanesh01 <psriganesh2005@gmail.com>
2025-03-26 01:37:25 +0530

    Fixed unnecessasory stuff

--- src/pages/Bot.jsx
+++ src/pages/Bot.jsx
@@ -1,17 +1,25 @@
 function Bot() {
-    return (
-      <div className="flex flex-col items-center justify-center w-full h-screen">
-        {/* <iframe
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      {/* <iframe
           src={import.meta.env.VITE_REACT_APP_CHATBASE_URL}
           className="w-full h-full border-0"
           title="Chatbot"
         ></iframe> */}
 
-        <p>
-          hi bitches. Give me (Kushal) marks
-        </p>
-      </div>
-    );
-  }
-  
-  export default Bot;
+      <p>
+        Ask Me anything
+
+      </p>
+
+      <input
+        type="text"
+        placeholder="Type your question..."
+        className="w-3/4 p-2 border border-gray-300 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+
+    </div>
+  );
+}
+
+export default Bot;
