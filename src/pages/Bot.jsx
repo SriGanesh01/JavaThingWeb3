4b15ea2187f3879a03d9cff9413e636d08eeba7f
SriGanesh01 <psriganesh2005@gmail.com>
2025-03-26 01:29:22 +0530

    unnecessasory stuff

--- src/pages/Bot.jsx
+++ src/pages/Bot.jsx
@@ -1,14 +1,17 @@
 function Bot() {
     return (
       <div className="flex flex-col items-center justify-center w-full h-screen">
-        <iframe
+        {/* <iframe
           src={import.meta.env.VITE_REACT_APP_CHATBASE_URL}
           className="w-full h-full border-0"
           title="Chatbot"
-        ></iframe>
+        ></iframe> */}
+
+        <p>
+          hi bitches. Give me (Kushal) marks
+        </p>
       </div>
     );
   }
   
   export default Bot;
-  
